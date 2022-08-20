db0b5fd9bf61dc35c637fcdb53057b28ac8fe672
securtie54 <88200933+securtie54@users.noreply.github.com>
2022-08-20 21:18:58 +0300

    Update Wi-Fİ scanner.py

--- Wi-Fİ scanner.py
+++ Wi-Fİ scanner.py
@@ -4,26 +4,21 @@ import pandas
 import time
 import os
 
-# initialize the networks dataframe that will contain all access points nearby
+# yakındaki tüm erişim noktalarını içerecek ağ veri çerçevesini başlat
 networks = pandas.DataFrame(columns=["BSSID", "SSID", "dBm_Signal", "Channel", "Crypto"])
-# set the index BSSID (MAC address of the AP)
+# BSSID dizinini ayarlayın (AP'nin MAC adresi)
 networks.set_index("BSSID", inplace=True)
 
 def callback(packet):
     if packet.haslayer(Dot11Beacon):
-        # extract the MAC address of the network
         bssid = packet[Dot11].addr2
-        # get the name of it
         ssid = packet[Dot11Elt].info.decode()
         try:
             dbm_signal = packet.dBm_AntSignal
         except:
             dbm_signal = "N/A"
-        # extract network stats
         stats = packet[Dot11Beacon].network_stats()
-        # get the channel of the AP
         channel = stats.get("channel")
-        # get the crypto
         crypto = stats.get("crypto")
         networks.loc[bssid] = (ssid, dbm_signal, channel, crypto)
 
@@ -43,19 +38,14 @@ def change_channel():
         ch = ch % 14 + 1
         time.sleep(0.5)
 
-
 if __name__ == "__main__":
-    # interface name, check using iwconfig
     interface = "wlan0mon"
-    # start the thread that prints all the networks
     printer = Thread(target=print_all)
     printer.daemon = True
     printer.start()
-    # start the channel changer
     channel_changer = Thread(target=change_channel)
     channel_changer.daemon = True
     channel_changer.start()
-    # start sniffing
     sniff(prn=callback, iface=interface)
 
 
